7c9c4f8087578a5edb346e3f6f19435268be3d5f
foosinn <stefan@f2o.io>
2025-09-06 01:03:46 +0200

    fix: use TokenType enum instead of Lexer

--- backend/src/Doctrine/Functions/Date.php
+++ backend/src/Doctrine/Functions/Date.php
@@ -4,7 +4,7 @@ namespace App\Doctrine\Functions;
 
 use Doctrine\DBAL\Platforms\AbstractPlatform;
 use Doctrine\ORM\Query\AST\Functions\FunctionNode;
-use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\TokenType;
 use Doctrine\ORM\Query\Parser;
 use Doctrine\ORM\Query\SqlWalker;
 
@@ -19,12 +19,12 @@ class Date extends FunctionNode {
     }
 
     public function parse(Parser $parser): void {
-        $parser->match(Lexer::T_IDENTIFIER);
-        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $parser->match(TokenType::T_IDENTIFIER);
+        $parser->match(TokenType::T_OPEN_PARENTHESIS);
 
         $this->date = $parser->ArithmeticPrimary();
 
-        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+        $parser->match(TokenType::T_CLOSE_PARENTHESIS);
     }
 
     private function getFunctionByPlatform(AbstractPlatform $platform, string $date): string {
